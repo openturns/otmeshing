c7fb645bdbbcb842059da101c5f5b545620b2ee3
Julien Schueller <schueller@phimeca.com>
2026-03-21 15:32:45 +0100

    CMake: Enable TBB

--- cmake/Findcddlib.cmake
+++ cmake/Findcddlib.cmake
@@ -24,17 +24,45 @@
 
 find_path (CDDLIB_INCLUDE_DIR cdd.h PATH_SUFFIXES cddlib)
 
+# version
+set (_VERSION_FILE ${CDDLIB_INCLUDE_DIR}/cddtypes.h)
+if (EXISTS ${_VERSION_FILE})
+  #define ddf_DDVERSION   "Version 0.94m"
+  file (STRINGS ${_VERSION_FILE} _VERSION_LINE REGEX "#define dd_DDVERSION")
+  if (_VERSION_LINE)
+    string (REGEX REPLACE ".*\"Version (.*)\"" "\\1" cddlib_VERSION ${_VERSION_LINE})
+  endif ()
+endif ()
+
+# check version
+set (_CDDLIB_VERSION_MATCH TRUE)
+if (cddlib_FIND_VERSION AND cddlib_VERSION)
+  if (cddlib_FIND_VERSION_EXACT)
+    if (${cddlib_FIND_VERSION} VERSION_EQUAL ${cddlib_VERSION})
+    else()
+      set (_CDDLIB_VERSION_MATCH FALSE)
+    endif ()
+  else ()
+    if (${cddlib_FIND_VERSION} VERSION_GREATER ${cddlib_VERSION})
+      set (_CDDLIB_VERSION_MATCH FALSE)
+    endif ()
+  endif ()
+endif ()
+
 find_library (CDDLIB_LIBRARY NAMES cdd)
 
 set (CDDLIB_LIBRARIES ${CDDLIB_LIBRARY})
 set (CDDLIB_INCLUDE_DIRS ${CDDLIB_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(cddlib DEFAULT_MSG CDDLIB_LIBRARY CDDLIB_INCLUDE_DIRS)
+find_package_handle_standard_args(cddlib
+  REQUIRED_VARS CDDLIB_LIBRARY CDDLIB_INCLUDE_DIRS
+  VERSION_VAR cddlib_VERSION)
 
 mark_as_advanced (
   CDDLIB_LIBRARY
   CDDLIB_LIBRARIES
   CDDLIB_INCLUDE_DIR
-  CDDLIB_INCLUDE_DIRS)
+  CDDLIB_INCLUDE_DIRS
+  cddlib_VERSION)
 
